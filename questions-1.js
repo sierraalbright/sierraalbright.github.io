207a049d2a279b88c26312cb5e29cffe14602d67
Sierra Albright <59371303+sierraalbright@users.noreply.github.com>
2020-02-22 07:49:41 -0600

    Add files via upload

---
+++ questions-1.js
@@ -0,0 +1,8 @@
+var questions = [
+  ["Which of the following is not a valid JavaScript variable name?" , "2names" , "_first_and_last_names", "FirstAndLast", "all","1"],
+  ["What tag is an extension to HTML that can enclose any number of JavaScript statements?", "<SCRIPT>", "<BODY>", " <HEAD>", "<TITLE>","1"],
+  ["What is the correct JavaScript syntax to write “Hello World”?", "System.out.println(“Hello World”) ", "println (“Hello World”)", "document.write(“Hello World”)", "response.write(“Hello World”)","3"],
+  ["Inside which HTML element do we put the JavaScript?", "<js>", "<script>", "<scripting>", "<javascript>","2"],
+  [" JavaScript entities start with _______ and end with _________.", "Semicolon, colon", "Semicolon, Ampersand", "Ampersand, colon", "Ampersand, semicolon","4"],
+  ["JavaScript is interpreted by _________", " Client", " Server ", "Object", "None of the above","1"],
+  ];
